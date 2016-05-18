40577e62348acc4001a00d103879a2e6afd51099
Brendan Quinn <brendan.quinn@nextgearcapital.com>
2016-05-18 08:01:03 -0400

    add autoprefixer-loader to css and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: path.resolve('js'),
     entry: ['./app.es6'],
     output: {
-        path: path.resolve('build/'),
-        publicPath: '/public/assets/',
+        path: path.resolve('build/js/'),
+        publicPath: '/public/assets/js/',
         filename: 'bundle.js'
     },
     devServer: {
@@ -18,12 +16,12 @@ module.exports = {
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+                loader: 'style-loader!css-loader!autoprefixer-loader',
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
             },
             {
                 test: /\.es6$/,
@@ -35,9 +33,7 @@ module.exports = {
             }
         ],
     },
-    plugins: [
-        new ExtractTextPlugin("styles.css")
-    ],
+    plugins: [],
     resolve: {
         extensions: ['', '.js', '.es6'],
     },
